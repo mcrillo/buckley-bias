87a6088db87630c8895eb376db4b20283f5a3c9c
Marina Costa Rillo <marina.rillo@evobio.eu>
2017-10-19 15:17:43 +0200

    bias data ready

--- analysis/R/aux_functions/merge_morpho.R
+++ analysis/R/aux_functions/merge_morpho.R
@@ -6,17 +6,28 @@
 
 merge_morpho <- function(){
 
-  all.bias.size <- data.frame(area=double() , diam.max=double(), species=character(), sspname=character(), sample=integer(), dataset=character(), datasetAB=character())
+  all.bias.size <- data.frame(area=double(), diam.max=double(), area_max=double(),  area_log=double(), area_log_max=double(), total_ind=integer(),
+                                species=character(), sspname=character(), sample=integer(), dataset=character(), datasetAB=character())
   s <- c("A", "B")
-  dataset <- c("resample", "buckley")
+  dataset <- c("Re-sampling", "Buckley")
+  
+  ssp.names <- c("G.calida","G.conglobatus","G.crassaformis","S.dehiscens","N.dutertrei","G.falconensis",
+                 "G.glutinata","T.humilis","G.inflata","G.menardii","P.obliquiloculata","N.pachyderma",
+                 "G.ruber","G.rubescens","G.sacculifer","G.scitula","G.siphonifera","G.tenellus",
+                 "G.truncatulinoides","G.tumida","O.universa")
+  
+  ssp.grep <- c("calida","conglobatus","crassaformis","dehiscens","dutertrei","falconensis",
+                "glutinata","humilis","inflata","menardii","obliquiloculata","pachyderma",
+                "ruber","rubescens","sacculifer","scitula","siphonifera","tenellus",
+                "truncatulinoides","tumida", "universa") 
   
   # removing files of species that were present only in one dataset (A or B)
   remove <- c("dehiscens20B.csv","dehiscens25B.csv","dehiscens31B.csv","dehiscens46B.csv","elongatus20A.csv",       
               "elongatus25A.csv","elongatus27A.csv","elongatus31A.csv","elongatus46A.csv","menardii25B.csv",        
               "menardii31B.csv","obliquiloculata31B.csv","pachyderma27B.csv","pachyderma44B.csv","truncatulinoides31B.csv")
+
   
-  
-  for (i in 1:2){
+  for (i in 1:2){ # i = 1
     
     files.names <- list.files(path = "data/raw_data/morpho_R", pattern=paste(s[i],".csv", sep=""))
     files.names <- files.names[-which(files.names %in% remove)]
@@ -25,10 +36,16 @@ merge_morpho <- function(){
       data <- read.csv(paste("data/raw_data/morpho_R/",k, sep=""), header = TRUE, stringsAsFactors=FALSE)
       
       data <- cbind(data, 
+                    area_max = max(data$area),
+                    area_log = log(data$area),
+                    area_log_max = log(max(data$area)),
+                    total_ind = length(data[,1]),
                     species = gsub( "[1-9].*$", "", k ),
-                    sample = rep(as.numeric(str_extract(k, "[[:digit:]]+")), length(data[,1])), 
+                    sspname = ssp.names[grep(gsub( "[1-9].*$", "", k ),ssp.grep)],
+                    sample = as.numeric(str_extract(k, "[[:digit:]]+")), 
                     dataset = dataset[i],
-                    datasetAB = s[i])
+                    datasetAB = s[i]
+                    )
       
       all.bias.size <- rbind(all.bias.size, data)
       
@@ -36,7 +53,6 @@ merge_morpho <- function(){
     }
   }
   
-  all.bias.size <- cbind(all.bias.size, area_log = log(all.bias.size$area))
   write.csv(all.bias.size, file = "data/morpho_bias-analysis.csv",row.names=FALSE)  
   return(all.bias.size)
   
--- analysis/R/main_bias.R
+++ analysis/R/main_bias.R
@@ -12,20 +12,12 @@ sourceDirectory("./R/aux_functions", modifiedOnly=FALSE)
 # Colours for plots
 cores10 <- c("#00b900","#0037c6", "#ff7314", "#ff60ff","#86442b","#004d41", "#db0011","#00b5ff","#8000b2", "#d0be00")
 
-############
-### Data ###
-############
-
-# General required Data:
-morpho_size_distrib <- read.csv(file=c("data/morpho_size-distrib.csv"), header = TRUE, stringsAsFactors = FALSE)
 
 # Extracting info of re-sampled sediments
+morpho_size_distrib <- read.csv(file=c("data/morpho_size-distrib.csv"), header = TRUE, stringsAsFactors = FALSE)
 resamples_df <- suppressWarnings(get_resamples_info(morpho_size_distrib, overwrite = FALSE)) # creates: data/resample_info.csv & data/resample_info_morpho.csv     
 plot_map_resamples(resamples_df, overwrite = FALSE) # creates: output/resamples_map.pdf
 
-
-# Data for assemblage similarity analysis
-forcens_df <- get_forcens_subset(resamples_df,overwrite=FALSE) 
 
 
 ##############################
@@ -33,6 +25,7 @@ forcens_df <- get_forcens_subset(resamples_df,overwrite=FALSE)
 ##############################
 
 ### Data
+forcens_df <- get_forcens_subset(resamples_df,overwrite=FALSE) 
 # Merging species counts (assemblages) from (A) re-samples , (B) Buckley collection and (C) ForCenS data:
 assemb_counts_df <- get_abund_counts(forcens_df) # creates "data/counts_merged.csv"
 assemb_relat_df <- get_abund_relat(forcens_df) # creates "data/counts_merged_relat.csv"
@@ -49,7 +42,6 @@ plot_assemb_similarity(assemb_sim_list, cores10) # creates "output/assemb_simila
 
 
 
-
 #########################
 ### Size Distribution ###
 #########################
@@ -58,9 +50,8 @@ plot_assemb_similarity(assemb_sim_list, cores10) # creates "output/assemb_simila
 if(!file.exists("data/morpho_bias-analysis.csv")){
   resamp_morpho_df <- read.csv("data/resample_info_morpho.csv", header = TRUE, stringsAsFactors=FALSE)
   # zf_all <- get_buckley_zf(resamples_df) # all slides ZFs numbers from Buckley Collection used in the bias analysis
-  # creating one CSV for each species for each sample (data/morpho_R/.)
-  get_morpho_buckley(morpho_size_distrib, resamp_morpho_df)
-  get_morpho_bias() # requires CSV files from folder data/morpho_bias_buckley
+  get_morpho_buckley(morpho_size_distrib, resamp_morpho_df) # creates data/raw_data_morpho_R
+  get_morpho_bias() # requires CSV files from folder data/raw_data/morpho_bias_buckley
   morpho_df <- merge_morpho()
 }else{
   morpho_df <-read.csv("data/morpho_bias-analysis.csv", header = TRUE, stringsAsFactors=FALSE)
@@ -70,16 +61,7 @@ if(!file.exists("data/morpho_bias-analysis.csv")){
 
 
 
-ssp.names <- c("G.calida","G.conglobatus","G.crassaformis","S.dehiscens","N.dutertrei","G.falconensis",
-               "G.glutinata","T.humilis","G.inflata","G.menardii","P.obliquiloculata","N.pachyderma",
-               "G.ruber","G.rubescens","G.sacculifer","G.scitula","G.siphonifera","G.tenellus",
-               "G.truncatulinoides","G.tumida","O.universa")
 
-ssp.grep <- c("calida","conglobatus","crassaformis","dehiscens","dutertrei","falconensis",
-              "glutinata","humilis","inflata","menardii","obliquiloculata","pachyderma",
-              "ruber","rubescens","sacculifer","scitula","siphonifera","tenellus",
-              "truncatulinoides","tumida", "universa") 
-sspname = rep(ssp.names[grep(gsub( "[1-9].*$", "", k ),ssp.grep)], length(data[,1])), 
 
 
 
