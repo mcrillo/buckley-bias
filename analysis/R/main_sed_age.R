24c4e8cd6b6d36fb0a6e4494c811c17f7372aec4
Marina Costa Rillo <marina.rillo@evobio.eu>
2018-07-05 14:34:25 +0200

    data sets ready to merge

--- analysis/R/main_sed_age.R
+++ analysis/R/main_sed_age.R
@@ -1,4 +1,4 @@
-############################################################
+# Created by Marina Costa Rillo in 05/July/2018 
 rm(list=ls())
 
 setwd("/Users/marinacostarillo/Google Drive/PhD/Projects")
@@ -11,20 +11,44 @@ source("R/library.R")
 sourceDirectory("./R/aux_functions", modifiedOnly=FALSE)
 
 ### Data
+historical <- read.csv("data_age/historical_data.csv", header = TRUE)
 
-holocene_full <- read.csv("data_age/ForCenS_woa.csv", header = TRUE)
-lgm_full <- read.csv("data_age/LGM_MARGO_renamed.csv", header = TRUE)
-hist <- read.csv("data_age/historical_data.csv", header = TRUE)
 
-# Subsetting data to nearest neighbours
+if (!file.exists("data_age/ForCenS_subset.csv") | !file.exists("data_age/LGM_subset.csv")){
+  # Subsetting data to nearest neighbours
+  holocene_full <- read.csv("data_age/ForCenS_woa.csv", header = TRUE)
+  lgm_full <- read.csv("data_age/LGM_MARGO_renamed.csv", header = TRUE)
 
-i=1
+  lgm <- data.frame()
+  holocene <- data.frame()
 
-lgm_neighbs <- data.frame()
+  for (i in 1 : nrow(historical)){
+    point <- as.numeric(historical[i,c("Long","Lat")])
 
-atl <- find_neighbours(point = as.numeric(resamples_df[i,c("Long","Lat")]), findin = atlantic_lgm[,c("Long","Lat")], distance = 0)
-ip <- find_neighbours(point = as.numeric(resamples_df[i,c("Long","Lat")]), findin = indo_pacific_lgm[,c("Long","Lat")], distance = 0)
-pac <- find_neighbours(point = as.numeric(resamples_df[i,c("Long","Lat")]), findin = pacific_lgm[,c("Long","Lat")], distance = 0)
+    lgm_neighb <- find_neighbours(point, findin = lgm_full[,c("Long","Lat")], distance = 0)
+    lgm <- rbind(lgm, cbind(lgm_neighb, lgm_full[lgm_neighb$row_findin,]))
+
+    holocene_neighb <- find_neighbours(point, findin = holocene_full[,c("Longitude","Latitude")], distance = 0)
+    holocene <- rbind(holocene, cbind(holocene_neighb, holocene_full[holocene_neighb$row_findin,]))
+  }
+
+  write.csv(holocene, "data_age/ForCenS_subset.csv", row.names = F)
+  write.csv(lgm, "data_age/LGM_subset.csv", row.names = F)
+
+}else{
+  holocene <- read.csv("data_age/ForCenS_subset.csv", header = TRUE)
+  lgm <- read.csv("data_age/LGM_subset.csv", header = TRUE)
+}
+
+# Distance (in km) between historical sample and LGM and ForCenS nearest sample
+data.frame(lgm=lgm$distance/1000,holocene=holocene$distance/1000)
+
+### Merging data counts
+sort(names(holocene)[24:68])
+sort(names(lgm)[19:56])
+sort(names(historical)[13:46])
+
+# BEFORE MERGING, CHECK RSpade package and function: how do they want the data?!
 
 
 
@@ -38,6 +62,10 @@ forcens_df <- get_forcens_subset(resamples_df,overwrite=FALSE)
 # Merging species counts (assemblages) from (A) re-samples , (B) Buckley collection and (C) ForCenS data:
 assemb_counts_df <- get_abund_counts(forcens_df, overwrite = FALSE) # creates "data/counts_merged.csv"
 assemb_relat_df <- get_abund_relat(forcens_df, overwrite = FALSE) # creates "data/counts_merged_relat.csv
+
+
+
+
 ### Analysis Holocene
 # Calculating similarity index (based on Chao) for assemblages of Re-sampling X Buckley Collection X ForCenS
 assemb_sim_list <- get_assemb_similarity(assemb_counts_df, resamples_df$sample, overwrite = FALSE)
@@ -49,41 +77,6 @@ plot_assemb_similarity(assemb_sim_list, cores10, overwrite = FALSE) # creates "o
 
 
 ### Data LGM
-atlantic_lgm <- read.csv("data/lgm_data/MARGO_Atlantic_LGM_PF.csv", header = TRUE, stringsAsFactors=FALSE)
-indo_pacific_lgm <- read.csv("data/lgm_data/MARGO_IndoPac_LGM_PF.csv", header = TRUE, stringsAsFactors=FALSE)
-pacific_lgm <- read.csv("data/lgm_data/MARGO_Pacific_LGM_PF.csv", header = TRUE, stringsAsFactors=FALSE)
-
-indo_pacific_lgm <- indo_pacific_lgm [-which(indo_pacific_lgm[,c("Long")]>180),]
-
-## point: vector of two numbers (longitude, latitude) 
-## findin: a matrix of 2 columns (first one is longitude, second is latitude) 
-## distance: if 0 finds nearest neighbour, if a positive value (in meters) finds neighbours within a circle with the value radius
-
-i=1
-
-lgm_neighbs <- data.frame()
-
-atl <- find_neighbours(point = as.numeric(resamples_df[i,c("Long","Lat")]), findin = atlantic_lgm[,c("Long","Lat")], distance = 0)
-ip <- find_neighbours(point = as.numeric(resamples_df[i,c("Long","Lat")]), findin = indo_pacific_lgm[,c("Long","Lat")], distance = 0)
-pac <- find_neighbours(point = as.numeric(resamples_df[i,c("Long","Lat")]), findin = pacific_lgm[,c("Long","Lat")], distance = 0)
-
-
-if(which.min(c(atl$distance, ip$distance, pac$distance))==1){ # Atlantic
-  lgm_neighbs[i,] <- cbind(atl, atlantic_lgm[atl$row_findin,])
-}
-
-if(which.min(c(atl$distance, ip$distance, pac$distance))==2){ # Indo-Pacific
-  
-}
-
-if(which.min(c(atl$distance, ip$distance, pac$distance))==3){ # Pacific
-  
-}
-
-
-
-# Old
-
 lgm_df <- get_abund_relat_lgm(overwrite=FALSE)
 
 ### Graph Holocene vs. LGM 
